81674250704323a91fc0775d600b5034a662af8d
Alexander D. <maniac@pzskc383.dp.ua>
2017-09-27 00:49:21 +0300

    sum fixez

--- build-vm.sh
+++ build-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OPENBSD_MIRROR=${OPENBSD_MIRROR:-http://ftp.piotrkosoft.net}
+OPENBSD_MIRROR=${OPENBSD_MIRROR:-http://ftp.bytemine.net}
 OPENBSD_ANONCVS=${OPENBSD_ANONCVS:-anoncvs.eu.openbsd.org}
 INSTALL_SOURCE=${INSTALL_SOURCE:-minimal}
 
