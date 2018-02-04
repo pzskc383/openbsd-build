6a10b038a5d2c261f36afce0d9a674c706ece354
Alexander D. <maniac@pzskc383.dp.ua>
2018-02-04 18:17:48 +0200

    other changes

--- build-vm.sh
+++ build-vm.sh
@@ -22,27 +22,19 @@ select_cvs_mirror() {
   echo anoncvs.eu.openbsd.org
 }
 
-setup_serv() {
-  [ -d ./serve ] || mkdir ./serve
+download_minirootfs() {
+  [ -e miniroot.fs ] && return
+  wget "$1/SHA256" || err "dowloading $fn from $1/$fn failed!"
+  wget "$1/miniroot${2}.fs" || err "dowloading $fn from $1/$fn failed!"
+  sha256sum -c --ignore-missing SHA256 || err "checksum failed"
+  rm SHA256
+  mv "miniroot${2}.fs" miniroot.fs
+}
 
-  for fn in pxeboot bsd.rd SHA256 ; do
-    [ -e "./serve/$fn" ] && continue
-    wget "$1/$fn" -O "./serve/$fn" || err "dowloading $fn from $1/$fn failed!"
-  done
 
-  [ -L "./serve/auto_install" ] || ln -s pxeboot ./serve/auto_install
 
-  ( { cd ./serve && sha256sum -c --ignore-missing SHA256; } || \
-    err "checksum failed" )
-
-  ( rm -rf ./serve/etc >/dev/null && mkdir ./serve/etc ) || \
-    err "can't clean /etc serv dir"
-
-  dd if=/dev/urandom bs=256 count=1 of=./serve/etc/random.seed >/dev/null 2>&1
-  echo boot tftp:bsd.rd > ./serve/etc/boot.conf
-
-  eval "echo \"$(cat "./templates/install-${VERSION}.tpl.sh")\"" \
-    > ./serve/install.conf
+template_install() {
+  eval "echo \"$(cat "./templates/install-${VERSION}.tpl.sh")\""
 }
 
 create_new_image() {
@@ -50,55 +42,19 @@ create_new_image() {
   qemu-img create -f qcow2 "$1" 10240M
 }
 
-run_http_serv() {
-  fifo=$(pwd)/http
-  [ -p "${fifo}.in" ] || mkfifo "${fifo}.in"
-  [ -p "${fifo}.out" ] || mkfifo "${fifo}.out"
-
-  set -x
-
-  ( LF=$(printf "%b_" '\r'); LF=${LF%_}
-
-  exec 3<"${fifo}.out" 4>"${fifo}.in"
-
-  while : ; do
-    read request <&3
-    file="${request#GET*/}"
-    file="${file% HTTP/*}"
-    file="${file%\?*}"
-
-    bd-log requested file "${file}"
-
-    while : ; do
-      read header
-      header=${header%$LF}
-      [ "$header" = "" ] && break;
-    done
-
-    if [ -r "./serve/$file" ] ; then
-      printf "%s\r\n%s\r\n\r\n" "HTTP/1.0 200 OK" "Content-type: text/plain" >&4
-      cat "./serve/$file" >&4
-      printf "\r\n" >&4
-      bd-log served file "${file}"
-    else
-      printf "%s\r\n\r\n" "HTTP/1.0 404 NOT FOUND" >&4
-      bd-log file "${file}" not found
-    fi
-
-  done ) &
-  serv_pid=$!
-  trap 'kill $serv_pid' EXIT
+chat_serial() {
+  [ -p serial ] || mkfifo ./serial
+  exec 3&<serial 
+  done=0
+  while [ $done -eq 0 ]; do
+    read -r line <&3
+    echo line: $line >&2
+  done
 }
-
-if [ $# -eq 1 ] && [ "$1" = -http ]; then
-  bd-log hi
-  exit
-fi
-
 
 MIRROR=${MIRROR:-$(select_file_mirror)}
 ANONCVS=${ANONCVS:-$(select_cvs_mirror)}
-FLAVOUR=${FLAVOUR:-release}
+FLAVOUR=${FLAVOUR:-current}
 
 if [ "$FLAVOUR" = current ] ; then
   VERSION=6.2
@@ -111,20 +67,19 @@ else
 fi
 export MIRROR MIRROR_PATH ANONCVS FLAVOUR VERSION
 
-setup_serv "$MIRROR/$MIRROR_PATH"
-#create_new_image obsd-build.img
-run_http_serv &
+create_new_image build.img
+download_minirootfs "$MIRROR/$MIRROR_PATH" "$(echo "$VERSION"|tr -d '.')"
+chat_serial &
 
-
-  #-object filter-dump,id=usernetdump,netdev=usernet,file=./usernet.pcap \
-#strace -o qemu.strace -f \
 qemu-system-x86_64 \
   -machine type=pc,accel=kvm \
   -m 512M \
-  -boot once=n \
-  -drive file=obsd-build.img,if=virtio,cache=writeback,discard=ignore,format=qcow2 \
-  -chardev pipe,id=pipedev,path=./http \
-  -netdev 'user,id=user-net0,tftp=./serve/,bootfile=auto_install,guestfwd=:10.0.2.2:80-chardev:pipedev' \
+  -boot once=d \
+  -device virtio-serial-pci -chardev pipe,id=ch0,path=serial,mux=on  \
+  -device virtserialport,chardev=ch0,name=serial0 \
+  -drive file=build.img,if=virtio,cache=writeback,discard=ignore,format=qcow2 \
+  -drive file=miniroot.fs,format=raw \
+  -netdev user,id=user-net0 \
   -device virtio-net-pci,netdev=user-net0 \
-  -display sdl \
-  -name obsd-build
+  -nographic
+
