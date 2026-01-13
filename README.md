f70e7c0750741c5b37ac1d01e76bce2d7236d639
Alexander D. <maniac@pzskc383.net>
2026-01-13 02:41:13 +0200

    cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Packer + Vagrant + OpenBSD
+
+Packer script to build OpenBSD vagrant VMs
--- packer/Vagrantfile.base.rb
+++
@@ -1,8 +0,0 @@
-Vagrant.require_version '>= 1.1.0'
-
-Vagrant.configure(2) do |config|
-  config.ssh.shell = 'ksh'
-  config.ssh.sudo_command = 'doas %c'
-
-  config.vm.synced_folder '.', '/vagrant', disabled: true
-end
